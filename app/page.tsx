b51ca05a285d917297064a35c18037805d448aa2
Javasrabni <javasrabnii@gmail.com>
2025-12-07 12:54:10 +0700

    setup4

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
 import Link from "next/link";
 
 export default function Home() {
+  const vibrate = async () => {
+    await Haptics.impact({
+      style: ImpactStyle.Medium
+    })
+  }
   return (
     <div className="m-auto max-w-[32rem] w-full bg-blue-400">
       <p>tes</p>
       <Link href="/auth/register">LOGIN</Link>
+      <button onClick={vibrate}>GETER</button>
     </div>
   );
 }
